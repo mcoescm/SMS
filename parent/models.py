cb9022876434e62648adbeae0ec60b9e6a2a35ec
flank <ankushgochke@gmail.com>
2018-08-15 11:16:06 -0700

    Ankush changed this

--- parent/models.py
+++ parent/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class parentreg(models.Model):
+    fnm = models.CharField(max_length=40)
+    mnm = models.CharField(max_length=40)
+    lnm = models.CharField(max_length=40)
+    address = models.CharField(max_length=400)
+    state = models.CharField(max_length=40)
+    city = models.CharField(max_length=40)
+    pin = models.CharField(max_length=40)
+    gender = models.CharField(max_length=40)
+    dob = models.DateField()
+    age = models.CharField(max_length=3)
+    occ = models.CharField(max_length=40)
+    phno = models.CharField(max_length=10)
+    unm = models.CharField(max_length=40)
+    email = models.CharField(max_length=40)
+    pwd = models.CharField(max_length=40)
