728f50c266a70961d34a6a84869dadaf1d2ab650
pavan <belekarpavan@gmail.com>
2018-08-16 00:40:11 +0530

    create student module database

--- student/urls.py
+++ student/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 
 from . import views
+app_name="student"
 
 urlpatterns = [
     path('', views.studreg, name='student_register'),
+    path('stud_save/', views.studsave, name='Student_save'),
     ]
 
