91fa0ff6b8cc3f0a4bbb8e965dde4b47b872a970
flank <ankushgochke@gmail.com>
2018-08-17 11:05:19 -0700

    Attendace Module Working 100%

--- attendance/models.py
+++ attendance/models.py
@@ -1,2 +1,11 @@
+
 from django.db import models
+from course.models import course
+
+
+class test(models.Model):
+    sid = models.IntegerField(12)
+    ispresent = models.IntegerField(12, default=0)
+    ccid = models.ForeignKey(course,on_delete=models.CASCADE, null=True)
+    dt = models.CharField(max_length=40)
 
--- attendance/views.py
+++ attendance/views.py
@@ -4,16 +4,20 @@ from django.http import HttpResponse
 from teacher.models import teacher
 from student.models import student
 from course.models import course
-def index(reqest):
-    obj = teacher.objects.get(email="ankushgochke@gmail.com")
+from .models import test
+
+def index(request):
+    em = request.session['user']
+    obj = teacher.objects.get(email= em)
     dict = {"data": obj}
-    return render(reqest,"attendance/attendance.html", dict)
+    return render(request,"attendance/attendance.html",dict)
 
 
 
 
 def findstudents(request):
-    obj3 = teacher.objects.get(email="ankushgochke@gmail.com")
+    em = request.session['user']
+    obj3 = teacher.objects.get(email=em)
 
 
     cs = request.POST['crs']
@@ -22,4 +26,28 @@ def findstudents(request):
     obj = student.objects.all().filter(course_id=cs)
     context = {"students":obj,"cnm" : obj2,"data": obj3}
     return render(request, "attendance/attendance.html", context)
+
+def store (request):
+    css = request.GET.get('ccid')
+    cdt = request.GET.get('dt')
+    obj = student.objects.all().filter(course_id=css)
+
+    dict = {"Sid": obj}
+
+    list=request.GET.getlist("att")
+
+
+
+    for i in obj:
+        obj2 = test()
+        obj2.sid = i.id
+        obj2.ccid_id = css
+        obj2.dt = cdt
+        obj2.save()
+
+    var = ""
+
+    for i in list:
+        test.objects.filter(sid = i).update(ispresent=1)
+
 
